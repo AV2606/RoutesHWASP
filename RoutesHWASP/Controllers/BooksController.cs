e8e3a38d9f365caacf4fe9388e821683fdf2765e
Avichay Vaknin <79922086+AV2606@users.noreply.github.com>
2022-05-28 15:26:30 +0300

    Add project files.

---
+++ RoutesHWASP/Controllers/BooksController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RoutesHWASP.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        [HttpGet("{id:min(6)}")]
+        public IActionResult GetValue(int id)
+        {
+            return Ok("Hello from Int GetValue");
+        }
+
+        [HttpGet("{str:regex(^((books)(zoom|boom))$)}")]
+        public IActionResult GetValue(string str)
+        {
+            return Ok("Hello from Regex GetValue");
+        }
+
+
+    }
+}
